23dcef5f3be48cfbfe23f7575954cd022da49054
Andrew Bruss <apb61@case.edu>
2013-04-08 15:38:16 -0400

    probability map methods

--- Assignment4/ProbabilityMap.java
+++ Assignment4/ProbabilityMap.java
@@ -4,7 +4,8 @@ public class ProbabilityMap{
 	private double[][] map;
 	private double towerAccuracy = .75;
 	private int towerRange = 5;
-	
+	private static int SHOOTING_RANGE = 5;
+	private static int TREE_RANGE = 2;
 	
 	public ProbabilityMap(int x, int y){
 		map = new double[x][y];
@@ -21,7 +22,29 @@ public class ProbabilityMap{
 	}
 	
 	public double probOfBeingShot(Location loc){
-		return 0.0;
+		int lowerx = loc.x-SHOOTING_RANGE;
+		int lowery = loc.y-SHOOTING_RANGE;
+		int upperx = loc.x+SHOOTING_RANGE;
+		int uppery = loc.y+SHOOTING_RANGE;
+		if(lowerx < 0)
+			lowerx = 0;
+		if(lowery < 0)
+			lowery =0;
+		if(upperx >= map.length)
+			upperx = map.length - 1;
+		if(uppery >= map[0].length)
+                        uppery = map[0].length - 1;
+
+		double tmp=1.0;
+		for(int i=lowerx; i<=upperx; i++){
+			for(int j=lowery; j<uppery; j++){
+				tmp = tmp*(1.0- getProbability(new Location(i, j)));
+			} 
+		}
+		tmp = 1.0-tmp;
+		
+		tmp = .75*tmp;
+		return tmp;
 	}
 	
 	public void setProbability(Location loc, double value){
@@ -34,7 +57,31 @@ public class ProbabilityMap{
 	
 	public void treeSeen(Location loc){
 		map[loc.x][loc.y] = 0;
+		int lowerx = loc.x-TREE_RANGE;
+                int lowery = loc.y-TREE_RANGE;
+                int upperx = loc.x+TREE_RANGE;
+                int uppery = loc.y+TREE_RANGE;
+                if(lowerx < 0)
+                        lowerx = 0;
+                if(lowery < 0)
+                        lowery =0;
+                if(upperx >= map.length)
+                        upperx = map.length - 1;
+                if(uppery >= map[0].length)
+                        uppery = map[0].length - 1;
+		Location current = null;
+                for(int i=lowerx; i<=upperx; i++){
+                        for(int j=lowery; j<uppery; j++){
+				current = new Location(i, j);
+				if(getProbability(current) == 0.0 || getProbability(current) == 1.0){
+					continue;
+				}
+				setProbability(new Location(i, j), Math.max(getProbability(loc) + 0.1, 1.0));
+                        }
+                }
+		setProbability(loc, 0.0);
 		//System.out.println("Tree seen");
+		return;
 	}
 	
 	public void emptySquare(Location loc){
@@ -42,6 +89,28 @@ public class ProbabilityMap{
 	}
 	
 	public void wasShot(Location loc){
+		int lowerx = loc.x-SHOOTING_RANGE;
+                int lowery = loc.y-SHOOTING_RANGE;
+                int upperx = loc.x+SHOOTING_RANGE;
+                int uppery = loc.y+SHOOTING_RANGE;
+                if(lowerx < 0)
+                        lowerx = 0;
+                if(lowery < 0)
+                        lowery =0;
+                if(upperx >= map.length)
+                        upperx = map.length - 1;
+                if(uppery >= map[0].length)
+                        uppery = map[0].length - 1;
+		Location current=null;
+		for(int i=lowerx; i<=upperx; i++){
+                        for(int j=lowery; j<uppery; j++){
+                                current = new Location(i, j);
+                                if(getProbability(current) == 0.0 || getProbability(current) >= 0.7){
+                                        continue;
+                                }
+                                setProbability(new Location(i, j), Math.max(getProbability(loc) + 0.2, 0.7));
+                        }
+                }
 		//System.out.println(loc.toString() + " Was Shot");
 	}
 	
